ac8e164037fafdfca387c9609da8465e452d5ce4
イーワ・チエン <kecumemoon@gmail.com>
2018-07-25 22:39:30 -0700

    Add files via upload

---
+++ playground.swift
@@ -0,0 +1,14 @@
+import Foundation 
+
+print("Hello World")
+
+var airports = ["TOR": "Toronto", "NY": "New York"]
+for (airportCode, airportName) in airports {
+   print("\(airportCode): \(airportName)")
+}
+
+
+var magazines = ["I": "IdN", "M": "Milk", "T": "The Little Things"]
+for (magazineCode, magazineName) in magazines {
+    print("\(magazineCode): \(magazineName)")
+}
